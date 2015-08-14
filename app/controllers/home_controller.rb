6f93bed373127ff87040b57baaeb167539b7510e
Abiel Gutierrez <abiel@carbonfive.com>
2015-08-14 16:43:38 -0700

    wip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  def pdf
-    resume = File.join(Rails.root, 'app/assets/Abiel_Gutierrez_Resume.pdf')
-    send_file(resume, :filename => "Abiel_Gutierrez_Resume.pdf", :disposition => 'inline', :type => "application/pdf")
-  end
-
-  helper_method :pdf
-
 end
